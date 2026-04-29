9643ed8b0ece700c2de1f5110191d3f94c268737
Your Name <your-email@example.com>
2026-04-29 22:55:05 +0530

    Initial commit: Spring AI ChatBot project

---
+++ src/main/java/com/spring/ai/firstproject/Controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.spring.ai.firstproject.Controller;
+
+
+import com.spring.ai.firstproject.Service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private ChatService chatservice;
+
+    public ChatController(ChatService chatservice){
+        this.chatservice= chatservice ;
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message){
+        return chatservice.getResponse(message) ;
+
+    }
+}
